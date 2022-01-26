08c413ca46c179bc528af0dcc0657beca6683346
Jose Alejandro <jscuesta96@gmail.com>
2022-01-27 00:05:15 +0100

    nutrient goal page

--- onboarding/onboarding_domain/src/main/java/com/example/onboarding_domain/use_case/ValidateNutrients.kt
+++ onboarding/onboarding_domain/src/main/java/com/example/onboarding_domain/use_case/ValidateNutrients.kt
@@ -9,21 +9,32 @@ class ValidateNutrients {
         carbsRatioText: String,
         proteinRatioText: String,
         fatRatioText: String
-    ): Unit{
+    ): Result {
         val carbsRatio = carbsRatioText.toIntOrNull()
         val proteinRatio = proteinRatioText.toIntOrNull()
         val fatRatio = fatRatioText.toIntOrNull()
 
         if(carbsRatio == null || proteinRatio == null || fatRatio == null){
-
+            return  Result.Error(
+                message = UiText.StringResource(com.example.core.R.string.error_invalid_values)
+            )
         }
+        if(carbsRatio + proteinRatio + fatRatio != 100){
+            return  Result.Error(
+                message = UiText.StringResource(com.example.core.R.string.error_not_100_percent)
+            )
+        }
+        return Result.Success(
+            carbsRatio / 100f,
+            proteinRatio / 100f,
+            fatRatio / 100f)
     }
 
     sealed class Result{
         data class Success(
-            val carbsRatio: Int,
-            val proteinRatio: Int,
-            val fatRatio: Int
+            val carbsRatio: Float,
+            val proteinRatio: Float,
+            val fatRatio: Float
         ): Result()
         data class Error(val message: UiText): Result()
     }
--- onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/nutrient_goal/NutrientGoalViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/nutrient_goal/NutrientGoalViewModel.kt
@@ -4,16 +4,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.core.domain.preferences.Preferences
 import com.example.core.domain.use_case.FilterOutDigits
+import com.example.core.navigation.Route
 import com.example.core.util.UiEvent
+import com.example.onboarding_domain.use_case.ValidateNutrients
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class NutrientGoalViewModel @Inject constructor(
     private val preferences: Preferences,
-    private val filterOutDigits: FilterOutDigits
+    private val filterOutDigits: FilterOutDigits,
+    private val validateNutrients: ValidateNutrients
 ) : ViewModel(){
 
     var state by mutableStateOf(NutrientGoalState())
@@ -41,7 +48,26 @@ class NutrientGoalViewModel @Inject constructor(
                 )
             }
             is NutrientGoalEvent.OnNextClick ->{
-
+                val result = validateNutrients(
+                    carbsRatioText = state.carbsRatio,
+                    proteinRatioText = state.proteinRatio,
+                    fatRatioText = state.fatRatio
+                )
+                when(result){
+                    is ValidateNutrients.Result.Success -> {
+                        preferences.saveCarbRatio(result.carbsRatio)
+                        preferences.saveProteinRatio(result.proteinRatio)
+                        preferences.saveFatRatio(result.fatRatio)
+                        viewModelScope.launch {
+                            _uiEvent.send(UiEvent.Navigate(Route.TRACKER_OVERVIEW))
+                        }
+                    }
+                    is ValidateNutrients.Result.Error ->{
+                        viewModelScope.launch {
+                            _uiEvent.send(UiEvent.ShowSnackbar(result.message))
+                        }
+                    }
+                }
             }
         }
     }
