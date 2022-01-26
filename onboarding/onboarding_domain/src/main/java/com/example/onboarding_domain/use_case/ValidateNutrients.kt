ae1d7b20a28d14a1055985aabf9bde47071d0c78
Jose Alejandro <34602875+JoseAlejandro96@users.noreply.github.com>
2022-01-26 18:35:31 +0100

    medio del trabajo

--- onboarding/onboarding_domain/src/main/java/com/example/onboarding_domain/use_case/ValidateNutrients.kt
+++ onboarding/onboarding_domain/src/main/java/com/example/onboarding_domain/use_case/ValidateNutrients.kt
@@ -1,4 +1,30 @@
 package com.example.onboarding_domain.use_case
 
+import com.example.core.util.UiText
+import com.example.onboarding_domain.R
+
 class ValidateNutrients {
+
+    operator fun invoke(
+        carbsRatioText: String,
+        proteinRatioText: String,
+        fatRatioText: String
+    ): Unit{
+        val carbsRatio = carbsRatioText.toIntOrNull()
+        val proteinRatio = proteinRatioText.toIntOrNull()
+        val fatRatio = fatRatioText.toIntOrNull()
+
+        if(carbsRatio == null || proteinRatio == null || fatRatio == null){
+
+        }
+    }
+
+    sealed class Result{
+        data class Success(
+            val carbsRatio: Int,
+            val proteinRatio: Int,
+            val fatRatio: Int
+        ): Result()
+        data class Error(val message: UiText): Result()
+    }
 }
