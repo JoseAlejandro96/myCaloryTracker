5fb6084ebd6330b10b77b49ef89022ed765ea1e6
Jose Alejandro <jscuesta96@gmail.com>
2022-01-31 22:55:23 +0100

    use cases 2

--- tracker/tracker_domain/src/main/java/com/example/tracker_domain/use_case/CalculateMealNutrients.kt
+++ tracker/tracker_domain/src/main/java/com/example/tracker_domain/use_case/CalculateMealNutrients.kt
@@ -1,11 +1,16 @@
 package com.example.tracker_domain.use_case
 
 import com.example.tracker_domain.model.MealType
+import com.example.tracker_domain.model.TrackedFood
 import java.util.prefs.Preferences
 
 class CalculateMealNutrients(
     private val preferences: Preferences
 ) {
+
+    operator fun invoke(trackedFoods: List<TrackedFood>): Unit{
+
+    }
 
     data class MealNutrients(
         val carbs: Int,
@@ -13,5 +18,17 @@ class CalculateMealNutrients(
         val fat: Int,
         val calories: Int,
         val mealType: MealType
+    )
+
+    data class Result(
+        val carbsGoal: Int,
+        val proteinGoal: Int,
+        val fatGoal: Int,
+        val caloriesGoal: Int,
+        val totalCarbs: Int,
+        val totalProtein: Int,
+        val totalFat: Int,
+        val totalCalories: Int,
+        val mealNutrients: Map<MealType, MealNutrients>
     )
 }
